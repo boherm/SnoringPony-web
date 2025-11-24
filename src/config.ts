0e2f57f3a8996cb576599603561f75bf6b604526
boherm <18699562+boherm@users.noreply.github.com>
2025-11-24 01:01:03 +0100

    Add fetch releases with github api

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,21 @@
 const Config = {
-    comingSoon: true,
-    appRepository: "https://github.com/boherm/SnoringPony",
+    comingSoon: false,
+    projectName: "SnoringPony",
+    github: {
+        owner: "boherm",
+        repo: "SnoringPony",
+        getUrl() {
+            return `https://github.com/${this.owner}/${this.repo}`;
+        },
+        getDownloadUrl(version: string, platform: string, extension: string) {
+            return this.getUrl() + `/releases/download/${version}/${Config.projectName}-${platform}-${version}.${extension}`;
+        },
+        getChangeLogUrl(version: string) {
+            return this.getUrl() + `/releases/tag/${version}`;
+        },
+        getApiReleases() {
+            return `https://api.github.com/repos/${this.owner}/${this.repo}/releases?per_page=100`;
+        }
+    }
 };
 export default Config;
--- src/pages/api/check-update.ts
+++ src/pages/api/check-update.ts
@@ -1,3 +1,12 @@
+import Config from "@config";
+
+const formatVersion = (data: any) => {
+    return {
+        version: data.tag_name,
+        changelog: data.body ? data.body.split('\n') : []
+    };
+};
+
 export async function GET() {
 
     const data = {
@@ -5,18 +14,40 @@ export async function GET() {
         winExtension: "exe",
         osxExtension: "pkg",
         linuxExtension: "AppImage",
-        // betaversion: {
-        //     version: "1.9.23b13",
-        //     changelog: ["Bonjour", "Ceci est", "Un test"]
-        // },
-        // stableversion: {
-        //     version: "1.9.24",
-        //     changelog: ["coucou", "prout", "test"]
-        // },
-        // archives: [
-        //     { version: "1.0.0", changelog: ["first!"] },
-        // ]
+        betaversion: {},
+        stableversion: {},
+        archives: []
     };
+
+    const apiData = await fetch(Config.github.getApiReleases()).then(res => res.json());
+    let betaVersion = null;
+    let stableVersion = null;
+
+    for (let i = 0; i < apiData.length; i++) {
+        const release = apiData[i];
+        if (betaVersion === null && release.tag_name.includes('b')) {
+            betaVersion = release;
+        }
+        if (stableVersion === null && !release.tag_name.includes('b')) {
+            stableVersion = release;
+        }
+        if (betaVersion !== null && stableVersion !== null) {
+            break;
+        }
+    }
+
+    if (betaVersion) {
+        data.betaversion = formatVersion(betaVersion);
+    }
+    if (stableVersion) {
+        data.stableversion = formatVersion(stableVersion);
+    }
+
+    for (let i = apiData.length - 1; i >= 0; i--) {
+        if (apiData[i].tag_name === stableVersion?.tag_name) continue;
+        if (apiData[i].tag_name === betaVersion?.tag_name) continue;
+        data.archives.push(formatVersion(apiData[i]));
+    }
 
     return Response.json(data);
 }
